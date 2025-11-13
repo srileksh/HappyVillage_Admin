aae6bca6a9d6a2689fe87094d073098fc182fab8
SreelakshmiJ <sreelakshmij45@gmail.com>
2025-11-13 13:22:07 +0530

    Edited DemoReport

--- src/api/demoReportApi.js
+++ src/api/demoReportApi.js
@@ -1,27 +1,36 @@
 
-import api from "./axios";
+// import api from "./axios";
 
-export const getFilters = async () => {
-  const res = await api.get(`demo/filters`);
-  return res.data;
-};
+// export const getFilters = async () => {
+//   const res = await api.get(`demo/filters`);
+//   return res.data;
+// };
 
-export const getPanchayaths = async () => {
-  const res = await api.get(`demo/panchayaths`);
-  return res.data;
-};
+// export const getPanchayaths = async () => {
+//   const res = await api.get(`demo/panchayaths`);
+//   return res.data;
+// };
 
-export const getWards = async () => {
-  const res = await api.get(`demo/wards`);
-  return res.data;
-};
+// export const getWards = async () => {
+//   const res = await api.get(`demo/wards`);
+//   return res.data;
+// };
 
-export const getFilterValues = async (filter) => {
-  const res = await api.get(`demo/filter-values?filter=${filter}`);
-  return res.data;
-};
+// export const getFilterValues = async (filter) => {
+//   const res = await api.get(`demo/filter-values?filter=${filter}`);
+//   return res.data;
+// };
 
-export const generateReport = async (payload) => {
-  const res = await api.post(`demo/generate`, payload);
-  return res.data;
-};
+// export const generateReport = async (payload) => {
+//   const res = await api.post(`demo/generate`, payload);
+//   return res.data;
+// };
+import React from 'react'
+
+function demoReportApi() {
+  return (
+    <div>demoReportApi</div>
+  )
+}
+
+export default demoReportApi
